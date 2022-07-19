9522ccc0e5a9cab4a1fc212011d31f5b68496d07
4nThonyIO <gamalianton@gmali.com>
2022-07-19 16:46:51 +0300

    Initial

---
+++ Server/Server/Models/BaseCinemaRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    internal abstract class BaseCinemaRoom
+    {
+        protected int roomId;
+        protected string name;
+        protected Dictionary<(int Row, int Column), Seat> seats;
+    }
+}
---
+++ Server/Server/Models/Seat.cs
@@ -0,0 +1,25 @@
+﻿namespace Server.Models
+{
+    internal class Seat
+    {
+        bool isAvailable;
+        double price;
+
+        public Seat()
+        {
+            isAvailable = true;
+        }
+
+        public Seat(double price)
+        {
+            isAvailable = true;
+            this.price = price;
+        }
+
+        public Seat( Seat seat)
+        {
+            isAvailable = true;
+            this.price = seat.price;
+        }
+    }
+}
