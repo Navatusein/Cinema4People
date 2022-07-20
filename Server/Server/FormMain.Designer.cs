6962e82b02dbbc3f3cc0bb86d8cf185cd7f4dd36
4nThonyIO <gamalianton@gmali.com>
2022-07-20 15:32:26 +0300

    CinmRoom class + MainForm

--- Server/Server/FormMain.Designer.cs
+++ Server/Server/FormMain.Designer.cs
@@ -28,20 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbRooms = new System.Windows.Forms.ListBox();
+            this.NewRoomSettingsTable = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.rowIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.NewRoomSettingsTable)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1";
+            // 
+            // lbRooms
+            // 
+            this.lbRooms.FormattingEnabled = true;
+            this.lbRooms.Location = new System.Drawing.Point(13, 12);
+            this.lbRooms.Name = "lbRooms";
+            this.lbRooms.Size = new System.Drawing.Size(192, 329);
+            this.lbRooms.TabIndex = 11;
+            // 
+            // NewRoomSettingsTable
+            // 
+            this.NewRoomSettingsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.NewRoomSettingsTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rowIndex,
+            this.colAmount,
+            this.tbPrice});
+            this.NewRoomSettingsTable.Location = new System.Drawing.Point(18, 48);
+            this.NewRoomSettingsTable.Name = "NewRoomSettingsTable";
+            this.NewRoomSettingsTable.Size = new System.Drawing.Size(316, 272);
+            this.NewRoomSettingsTable.TabIndex = 31;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btAdd);
+            this.groupBox1.Controls.Add(this.NewRoomSettingsTable);
+            this.groupBox1.Location = new System.Drawing.Point(211, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 338);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new room";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Room name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 45;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(212, 18);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(105, 21);
+            this.btAdd.TabIndex = 44;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // rowIndex
+            // 
+            this.rowIndex.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.rowIndex.FillWeight = 111.9289F;
+            this.rowIndex.HeaderText = "Row Number";
+            this.rowIndex.Name = "rowIndex";
+            this.rowIndex.ReadOnly = true;
+            // 
+            // colAmount
+            // 
+            this.colAmount.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = "1";
+            this.colAmount.DefaultCellStyle = dataGridViewCellStyle1;
+            this.colAmount.FillWeight = 111.9289F;
+            this.colAmount.HeaderText = "Amount in this row";
+            this.colAmount.Name = "colAmount";
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.Format = "C2";
+            dataGridViewCellStyle2.NullValue = "0.01";
+            this.tbPrice.DefaultCellStyle = dataGridViewCellStyle2;
+            this.tbPrice.FillWeight = 76.14214F;
+            this.tbPrice.HeaderText = "Single seat price";
+            this.tbPrice.Name = "tbPrice";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(571, 356);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbRooms);
+            this.Controls.Add(this.label1);
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.NewRoomSettingsTable)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbRooms;
+        private System.Windows.Forms.DataGridView NewRoomSettingsTable;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowIndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tbPrice;
     }
 }
 
--- Server/Server/FormMain.cs
+++ Server/Server/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Server.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,20 @@ namespace Server
 {
     public partial class FormMain : Form
     {
+        List<CinemaRoom> rooms;
+        int counter = 0;
         public FormMain()
         {
             InitializeComponent();
+            btAdd.Enabled = false;
+            lbRooms.DataSource = rooms;
+            NewRoomSettingsTable.AllowUserToAddRows = true;
+            NewRoomSettingsTable.DefaultValuesNeeded += NewRoomSettingsTable_DefaultValuesNeeded1;
+        }
+
+        private void NewRoomSettingsTable_DefaultValuesNeeded1(object sender, DataGridViewRowEventArgs e)
+        {
+            NewRoomSettingsTable.Rows[e.Row.Index].Cells["rowIndex"].Value = e.Row.Index + 1;
         }
     }
 }
