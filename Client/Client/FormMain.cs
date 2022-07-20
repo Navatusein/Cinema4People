1e66e2cc958e7ed47463ca055446a1ed7b563442
Navatusein <boghdan.kutsulima@gmail.com>
2022-07-20 16:01:03 +0300

    Created the main form, a tab with films. An algorithm for displaying movie cards in the movies tab has been created. An adaptive template for movie cards has been created.

--- Client/Client/FormMain.cs
+++ Client/Client/FormMain.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Client.Tabs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,50 @@ namespace Client
 {
     public partial class FormMain : Form
     {
+        Button lastButton;
+
         public FormMain()
         {
             InitializeComponent();
+
+
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        private void buttonTabAfisha_Click(object sender, EventArgs e)
+        {
+            if (!(sender is Button))
+                return;
+            if (lastButton != null)
+                lastButton.BackColor = Color.Transparent;
+
+            lastButton = sender as Button;
+
+            lastButton.BackColor = Color.FromArgb(31, 31, 31);
+
+            panelForm.Controls.Clear();
+            TabAfisha tab = new TabAfisha();
+            panelForm.Controls.Add(tab);
+            tab.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(sender is Button))
+                return;
+
+            if (lastButton != null)
+                lastButton.BackColor = Color.Transparent;
+
+            lastButton = sender as Button;
+
+            lastButton.BackColor = Color.FromArgb(31, 31, 31);
+
+            lastButton = sender as Button;
+            panelForm.Controls.Clear();
+            TabFilm tab = new TabFilm();
+            panelForm.Controls.Add(tab);
+            tab.Show();
         }
     }
 }
---
+++ Client/Client/Tabs/TabFilm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Client.Tabs
+{
+    partial class TabFilm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelCardsDock = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelCardsDock.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 35);
+            this.panel1.TabIndex = 2;
+            // 
+            // panelCardsDock
+            // 
+            this.panelCardsDock.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelCardsDock.AutoScroll = true;
+            this.panelCardsDock.AutoScrollMargin = new System.Drawing.Size(0, 12);
+            this.panelCardsDock.Controls.Add(this.label1);
+            this.panelCardsDock.Controls.Add(this.panel3);
+            this.panelCardsDock.Controls.Add(this.panel2);
+            this.panelCardsDock.Location = new System.Drawing.Point(0, 35);
+            this.panelCardsDock.Name = "panelCardsDock";
+            this.panelCardsDock.Size = new System.Drawing.Size(580, 415);
+            this.panelCardsDock.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(231, 556);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel3.Location = new System.Drawing.Point(181, 157);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(247, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // TabFilm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(580, 450);
+            this.Controls.Add(this.panelCardsDock);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TabFilm";
+            this.Text = "TabFilm";
+            this.panelCardsDock.ResumeLayout(false);
+            this.panelCardsDock.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelCardsDock;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
