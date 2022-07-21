955e68667b84a69f5aa24bb82372d5d66ee76489
Yurii <yurko.frolyak@gmail.com>
2022-07-21 13:52:17 +0300

    Afisha 1version

---
+++ Server/Server/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Session
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string MovieTitle { get; set; }
+        public int Room { get; set; }
+        public UInt32 IncomeTicket { get; set; }
+        public UInt32 IncomeBar { get; set; }
+
+    }
+}
