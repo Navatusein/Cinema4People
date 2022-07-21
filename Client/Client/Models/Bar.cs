16948a4729c9ef05d46c5c3475efee099f054177
artem <artemonlypnyk@ukr.net>
2022-07-21 16:23:41 +0300

    Add class Bar and Product

--- Client/Client/Models/Bar.cs
+++ Client/Client/Models/Bar.cs
@@ -1,16 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using Client.Models;
 
 namespace Client
 {
     public class Bar
     {
-        public Product[] productRange { get; set; }
+        List<Product> _products;
+        double sumOfDay;
 
-        public void incomeCalculation()
+        public List<Product> Products { get => _products; private set => _products = value; }
+
+        public Bar()
         {
-            // TODO
+            _products = new List<Product>();
+            sumOfDay = 0;
         }
+
+        public Bar(List<Product> pr, double sod)
+        {
+            _products = pr;
+            sumOfDay = sod;
+        }
+
+        public bool isEnoughQuantity(Product p, int quantityOfProduct)
+        {
+            return quantityOfProduct <= _products[_products.IndexOf(p)].Quantity;
+        }
+
+
+        public double Buy(Product p, int quantityOfProduct)
+        {
+            _products[_products.IndexOf(p)].Quantity -= quantityOfProduct;
+            double tmp = _products[_products.IndexOf(p)].Price * quantityOfProduct;
+            sumOfDay += tmp;
+            return tmp;
+        }
+
     }
+
+
 }
 
--- Client/Client/Models/Product.cs
+++ Client/Client/Models/Product.cs
@@ -3,9 +3,88 @@ namespace Client.Models
 {
     public class Product
     {
-        public string Name { get; set; }
-        public float Price { get; set; }
-        public float Quantity { get; set; }
+        string _name;
+        float _price;
+        int _quantity;
+        string _image;
+
+        public Product(string name, float price, int quantity, string image)
+        {
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+            _image = image;
+        }
+
+        public Product()
+        {
+            _name = string.Empty;
+            _price = 0;
+            _quantity = 0;
+            _image = string.Empty;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _quantity = value;
+                }
+            }
+        }
+
+        public string Image
+        {
+            get
+            {
+                return _image;
+            }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _image = value;
+                }
+            }
+        }
     }
 }
 
