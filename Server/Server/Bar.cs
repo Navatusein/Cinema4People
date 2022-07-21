f3ad61b2af53ab3568e9cc3f922a22c0d5a51676
artem <artemonlypnyk@ukr.net>
2022-07-21 16:48:17 +0300

    Create design Bar Add Product

---
+++ Server/Server/Bar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Server.Models;
+
+namespace Server
+{
+    public partial class Bar : Form
+    {
+        List<Product> products;
+        private OpenFileDialog openFileDialog;
+
+        public Bar()
+        {
+            InitializeComponent();
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(nametb.Text) || string.IsNullOrWhiteSpace(pricetb.Text) || string.IsNullOrWhiteSpace(quantitytb.Text) || string.IsNullOrWhiteSpace(imagetb.Text))
+            {
+                MessageBox.Show("Enter all fields");
+                return;
+            }
+            Product tmp = new Product(nametb.Text, (float)Convert.ToDouble(pricetb.Text), Convert.ToInt32(quantitytb.Text), imagetb.Text);
+            products.Add(tmp);
+        }
+
+        private void imagebtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fn = openFileDialog.FileName;
+                    //var sr = new StreamReader(openFileDialog.FileName);
+                    //SetText(sr.ReadToEnd());
+                    imagetb.Text = fn;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
---
+++ Server/Server/Models/Product.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Product
+    {
+        string _name;
+        float _price;
+        int _quantity;
+        string _image;
+
+        public Product(string name, float price, int quantity, string image)
+        {
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+            _image = image;
+        }
+
+        public Product()
+        {
+            _name = string.Empty;
+            _price = 0;
+            _quantity = 0;
+            _image = string.Empty;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _quantity = value;
+                }
+            }
+        }
+
+        public string Image
+        {
+            get
+            {
+                return _image;
+            }
+
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _image = value;
+                }
+            }
+        }
+    }
+}
