602d29ba5fdb7382d093ce0a95fadcd331ba86ac
Саша Перетятько <sashoki9090@gmail.com>
2022-07-26 14:09:30 +0300

    Db

---
+++ Server/Server/ModelDB/CinemaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.ModelDB
+{
+    public class CinemaDbContext : DbContext
+    {
+
+    }
+}
---
+++ Server/Server/ModelDB/SessionDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.ModelDB
+{
+    public class SessionDb
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public int MovieId { get; set; }
+        public int RoomId { get; set; }
+        public UInt32 IncomeTicket { get; set; }
+        public UInt32 IncomeBar { get; set; }
+    }
+}
